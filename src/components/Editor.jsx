789e476bacd9d8e992e4c169580e8d6a383649ca
najmiter <realnajmiter@gmail.com>
2024-03-26 17:39:00 +0500

    i'm going through channnnnngez

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,6 +2,8 @@ import styles from "./Editor.module.css";
 
 export default function Editor() {
     return (
-        <div className={styles.editor}>Preview for the editable timetable</div>
+        <div className={`p2 ${styles.editor}`}>
+            Preview for the editable timetable
+        </div>
     );
 }
--- src/components/ToolsPanel.jsx
+++ src/components/ToolsPanel.jsx
@@ -2,7 +2,7 @@ import styles from "./ToolsPanel.module.css";
 
 export default function ToolsPanel() {
     return (
-        <div className={styles.toolsPanel}>
+        <div className={`${styles.toolsPanel} p2`}>
             Tools to customise the timetable
         </div>
     );
