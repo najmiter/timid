67ea5458a1edece68eda984e1fc411630f1b784d
najmiter <realnajmiter@gmail.com>
2024-03-29 01:20:51 +0500

    omg

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function getInitialSlots() {
 export default function App() {
     const [slots, setSlots] = useState(getInitialSlots());
     const [times, setTimes] = useState({
-        slotTime: 40,
-        startTime: "00:00",
+        slotTime: 90,
+        startTime: "08:45",
         break: true,
     });
 
