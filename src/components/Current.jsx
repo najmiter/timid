9bd1846b844737dea19ae473b58fe0d25901bc59
najmiter <realnajmiter@gmail.com>
2024-03-29 13:33:48 +0500

    more styles

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -17,7 +17,7 @@ export default function Current({ times, slots, searchParams }) {
                         null
                 ) ?? slots;
 
-            const today = new Date().getDay();
+            const today = 1;
             if (today !== 6 && today !== 0) {
                 const todaysSlots = {};
                 const cols = 7;
@@ -29,6 +29,8 @@ export default function Current({ times, slots, searchParams }) {
 
                 return todaysSlots;
             }
+
+            return "weekend";
         } catch (e) {
             console.error(e?.message);
             return slots;
@@ -54,71 +56,93 @@ export default function Current({ times, slots, searchParams }) {
             slotNumber += 1;
         }
 
-        return slotNumber ? slotNumber - 1 : slotNumber;
+        return slotNumber - 1;
     }
 
     return (
         <main className={styles.currentMain}>
             <div className={styles.dataWrapper}>
                 <h1 className={styles.tableTitle}>Timetable</h1>
-                <>
+                {slots_ === "weekend" ? (
                     <header className={styles.kheader}>
                         <h1 className={styles.currentHeadingSubject}>
-                            {slots_[currentLectureNumber]?.subject.length
-                                ? slots_[currentLectureNumber]?.subject
-                                : "FREE!"}
+                            IT&apos;S THE WEEKEND!
                         </h1>
-                        <h2 className={styles.currentHeadingRoom}>
-                            {slots_[currentLectureNumber]?.room.length
-                                ? slots_[currentLectureNumber]?.room
-                                : "GO HOME OR SOME SHIT"}
-                        </h2>
                     </header>
-                    <hr />
+                ) : (
+                    <>
+                        {currentLectureNumber !== -1 ? (
+                            <header className={styles.kheader}>
+                                <h1 className={styles.currentHeadingSubject}>
+                                    {slots_[currentLectureNumber]?.subject
+                                        .length
+                                        ? slots_[currentLectureNumber]?.subject
+                                        : "FREE!"}
+                                </h1>
+                                <h2 className={styles.currentHeadingRoom}>
+                                    {slots_[currentLectureNumber]?.room.length
+                                        ? slots_[currentLectureNumber]?.room
+                                        : "GO HOME OR SOME SHIT"}
+                                </h2>
+                            </header>
+                        ) : (
+                            <header className={styles.kheader}>
+                                <h2 className={styles.currentHeadingRoom}>
+                                    <span className="text-light">
+                                        CLASSES WILL START AT
+                                    </span>
+                                </h2>
+                                <h1 className={styles.currentHeadingSubject}>
+                                    {times.startTime}
+                                </h1>
+                            </header>
+                        )}
+                        <hr />
 
-                    <div className={styles.futureLectures}>
-                        <h2 className={styles.futureHeading}>
-                            Today&apos;s Lectures
-                        </h2>
-                        <ul>
-                            {futureLectures.map((_, i) => (
-                                <li key={i}>
-                                    <div
-                                        className={`${styles.futureLectureItem} ${
-                                            currentLectureNumber === i
-                                                ? styles.currentSlot
-                                                : i < currentLectureNumber
-                                                  ? styles.doneLecture
-                                                  : ""
-                                        } ${slots_[i]?.subject && slots_[i]?.room ? "" : styles.freeLecture}`}
-                                    >
-                                        {slots_[i]?.subject &&
-                                        slots_[i]?.room ? (
-                                            <>
-                                                <h3
-                                                    className={
-                                                        styles.futureLectureSubject
-                                                    }
-                                                >
-                                                    {slots_[i]?.subject}
-                                                </h3>
-                                                <h4
-                                                    className={
-                                                        styles.futureLectureRoom
-                                                    }
-                                                >
-                                                    {slots_[i]?.room}
-                                                </h4>
-                                            </>
-                                        ) : (
-                                            "FREE"
-                                        )}
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </>
+                        <div className={styles.futureLectures}>
+                            <h2 className={styles.futureHeading}>
+                                Today&apos;s Lectures
+                            </h2>
+                            <ul>
+                                {futureLectures.map((_, i) => (
+                                    <li key={i}>
+                                        <div
+                                            className={`${styles.futureLectureItem} ${
+                                                currentLectureNumber === i
+                                                    ? styles.currentSlot
+                                                    : i < currentLectureNumber
+                                                      ? styles.doneLecture
+                                                      : ""
+                                            } ${slots_[i]?.subject && slots_[i]?.room ? "" : styles.freeLecture}`}
+                                        >
+                                            {slots_[i]?.subject &&
+                                            slots_[i]?.room ? (
+                                                <>
+                                                    <h3
+                                                        className={
+                                                            styles.futureLectureSubject
+                                                        }
+                                                    >
+                                                        {slots_[i]?.subject}
+                                                    </h3>
+                                                    <h4
+                                                        className={
+                                                            styles.futureLectureRoom
+                                                        }
+                                                    >
+                                                        {slots_[i]?.room}
+                                                    </h4>
+                                                </>
+                                            ) : (
+                                                "FREE"
+                                            )}
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </>
+                )}
             </div>
         </main>
     );
